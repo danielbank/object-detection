63afe6c3b595eaa07702b8900cb8d59ee0629b91
Daniel Bank <daniel.p.bank@gmail.com>
2022-09-19 11:34:56 -0700

    One line change to ObjectDetectorHelper.kt to update the model path, that's all we need to do to use our new model

--- android/app/src/main/java/org/tensorflow/lite/examples/objectdetection/ObjectDetectorHelper.kt
+++ android/app/src/main/java/org/tensorflow/lite/examples/objectdetection/ObjectDetectorHelper.kt
@@ -82,14 +82,7 @@ class ObjectDetectorHelper(
 
         optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
 
-        val modelName =
-            when (currentModel) {
-                MODEL_MOBILENETV1 -> "mobilenetv1.tflite"
-                MODEL_EFFICIENTDETV0 -> "efficientdet-lite0.tflite"
-                MODEL_EFFICIENTDETV1 -> "efficientdet-lite1.tflite"
-                MODEL_EFFICIENTDETV2 -> "efficientdet-lite2.tflite"
-                else -> "mobilenetv1.tflite"
-            }
+        val modelName = "model.tflite"
 
         try {
             objectDetector =
